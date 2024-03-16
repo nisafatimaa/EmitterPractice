7c80165a131779a85a3d46dda48681e0eed0d561
Nisa Fatima <nisafatima0000@gmail.com>
2024-03-16 12:41:58 +0500

    Initial Commit

---
+++ EmitterPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmitterPractice
+//
+//  Created by Macbook Pro on 16/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
